5ea592d75a7f8f4028d5ddad46085b98781d3c9e
why <you@example.com>
2019-11-01 16:24:38 +0800

    账单明细

--- src/views/mine/billDetails.vue
+++ src/views/mine/billDetails.vue
@@ -1,3 +1,20 @@
 <template>
-    <div class="billDetails">账单明细</div>
+  <div class="billDetails">
+    <top-title>账单明细</top-title>
+    <div class="date-picker"></div>
+    <div class="bill-list">
+      <div class="item"></div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.billDetails{
+  min-height: 100vh;
+  background: #fff;
+}
+.date-picker{
+  height: 1.17333rem;
+  background: #efefef;
+}
+</style>

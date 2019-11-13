19c9ba3835dd4f339730d711346e1e1ce2617eb3
why <you@example.com>
2019-11-13 17:50:57 +0800

    文章组件

--- src/components/articleContent/articleContent.vue
+++ src/components/articleContent/articleContent.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="articleContent">
     <div class="sub-title">{{ articleInfo.subTitle }}</div>
-    <div class="date-wrap">
-      <div class="date-time"></div>
-      <div class="scan">
+    <div class="date-wrap clear-fix">
+      <div class="date-time left">2019-10-22 20:20</div>
+      <div class="scan right">
         <span class="icon"></span>
-        <span class="num"></span>
+        <span class="num">1234</span>
       </div>
     </div>
+    <div class="text-content">为什么说钓鱼人不容易得老年痴呆，不知道你信不信为什么说钓鱼人不容易得老年痴呆，不知道你信不信为什么说钓鱼人不容易得老年痴呆，不知道你信不信</div>
   </div>
 </template>
 
@@ -33,6 +34,26 @@ export default {
   .sub-title {
     font-size: 0.4rem;
     font-weight: 700;
+    line-height: 1.2em;
+  }
+  .date-wrap{
+    margin-top: .4rem;
+    font-size: .26667rem;
+    .date-time{
+    }
+    .scan{
+      .icon{
+        display: block;
+        width: .4rem;
+        height: .26667rem;
+        background: pink;
+        float: left;
+        margin-right: .13333rem;
+      }
+    }
+  }
+  .text-content{
+    margin-top: .4rem;
     line-height: 1.2em;
   }
 }

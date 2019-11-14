944e2fec0e0140c96fbd3250ebf9d7429807a0da
why <you@example.com>
2019-11-14 18:02:57 +0800

    消息

--- src/views/message/subPage/messageList.vue
+++ src/views/message/subPage/messageList.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="messageList">
     <top-title>{{ titleText }}</top-title>
-    <div class="content"></div>
+    <div class="content">
+      <div class="item">
+        <div class="avator">
+          <img :src="avator">
+        </div>
+        <div class="info">
+          <div class="name">用户-asasdasda</div>
+          <div class="text">
+            关注了您
+            <span>去主页看看</span>
+          </div>
+        </div>
+        <div class="date-time">10月26日 14:30</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +23,8 @@
 export default {
   data () {
     return {
-      titleText: '关注通知'
+      titleText: '关注通知',
+      avator: require('@/assets/avator2.jpeg')
     }
   },
   created () {
@@ -21,3 +36,38 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.content {
+  padding: 0 0.42667rem 0.42667rem;
+  .item {
+    padding: 0.42667rem 0;
+    display: flex;
+    .avator {
+      width: 1.06667rem;
+      height: 1.06667rem;
+      border-radius: 50%;
+      overflow: hidden;
+      background: pink;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .info {
+      padding: 0.08rem 0.21333rem;
+      flex: 1;
+      .text {
+        font-size: 0.34667rem;
+        color: #787878;
+        margin-top: 0.18667rem;
+      }
+    }
+    .date-time {
+      padding-top: 0.08rem;
+      font-size: 0.32rem;
+      color: #787878;
+    }
+  }
+}
+</style>

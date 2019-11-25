43bd76cd777a4ff8a1ef92c10b9993d62c8e7eed
why <you@example.com>
2019-11-25 16:48:10 +0800

    app跳转链接文档

---
+++ src/router/article/index.js
@@ -0,0 +1,12 @@
+const article = [
+  {
+    path: '/article/articleDetails',
+    name: 'articleDetails',
+    component: () => import('@/views/article/articleDetails'),
+    meta: {
+      title: 'articleDetails'
+    }
+  }
+]
+
+export default article

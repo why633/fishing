d997768d0d89daaf1b5f04095b16eb12b1251c76
why <you@example.com>
2019-12-04 11:11:24 +0800

    修改账单明细

--- src/views/mine/billDetails.vue
+++ src/views/mine/billDetails.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="billDetails">
     <top-title>账单明细</top-title>
-    <div :class="['date-picker',dateText == '请选择时间'?'default':'']" @click="pickDate">{{ dateText }}</div>
-    <div class="bill-list">
-      <div class="item" v-for="(item,index) in billData" :key="index">
-        <div class="left-info">
-          <div class="title">
-            {{item.description}}
-            <span class="sub-title">（{{item.status}}）</span>
+    <mescroll-vue ref="mescroll" :down="mescrollDown" :up="mescrollUp" @init="mescrollInit">
+      <div
+        :class="['date-picker',dateText == '请选择时间'?'default':'']"
+        @click="pickDate"
+      >{{ dateText }}</div>
+      <div class="bill-list" id="dataList">
+        <div class="item" v-for="(item,index) in billData" :key="index">
+          <div class="left-info">
+            <div class="title">
+              {{item.description}}
+              <span class="sub-title">（{{item.status}}）</span>
+            </div>
+            <div class="date">{{item.createTime|formateDateTime}}</div>
           </div>
-          <div class="date">{{item.createTime|formateDateTime}}</div>
-        </div>
-        <div class="right-info">
-          <span>{{item.amount}}</span>
+          <div class="right-info">
+            <span>{{item.amount}}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </mescroll-vue>
     <picker
       :data="pickData"
       :showToolbar="true"
@@ -23,7 +28,6 @@
       @confirm="confirm"
       :visible.sync="pickerVisible"
     />
-    <div class="no-data" v-if="billData.length==0">暂无数据...</div>
   </div>
 </template>
 
@@ -89,14 +93,64 @@ export default {
       pageNo: 1,
       pageSize: 10,
       totalCount: 0,
-      billData: []
+      billData: [],
+      mescroll: null, // mescroll实例对象
+      mescrollDown: {
+        auto: false,
+        isLock: true,
+        mustToTop: true
+      },
+      mescrollUp: {
+        auto: true,
+        offset: 10,
+        isBounce: false,
+        callback: this.upCallback, // 上拉回调,此处可简写; 相当于 callback: function (page, mescroll) { getListData(page); }
+        page: {
+          num: 0, // 当前页码,默认0,回调之前会加1,即callback(page)会从1开始
+          size: 10 // 每页数据的数量
+        },
+        htmlNodata: '<p class="upwarp-nodata">-- 无更多数据 --</p>',
+        noMoreSize: 10, // 如果列表已无数据,可设置列表的总数量要大于等于5条才显示无更多数据;避免列表数据过少(比如只有一条数据),显示无更多数据会不好看
+        // toTop: {
+        //   // src: './static/mescroll/mescroll-totop.png' // 回到顶部按钮的图片路径,支持网络图
+        // },
+        empty: {
+          // 列表第一页无任何数据时,显示的空提示布局; 需配置warpId才生效;
+          warpId: 'dataList', // 父布局的id;
+          // icon: './static/mescroll/mescroll-empty.png', // 图标,支持网络图
+          tip: '暂无相关数据~', // 提示
+          btntext: '' // 按钮,默认""
+          // btnClick () { // 点击按钮的回调,默认null
+          //   alert('点击了按钮,具体逻辑自行实现')
+          // }
+        },
+        lazyLoad: {
+          use: true // 是否开启懒加载,默认false
+        }
+      }
     }
   },
   created () {
     this.getPickerYearList()
-    this.getBillDetails()
   },
   methods: {
+    // mescroll组件初始化的回调,可获取到mescroll对象
+    mescrollInit (mescroll) {
+      this.mescroll = mescroll
+    },
+    // 上拉回调 page = {num:1, size:10}; num:当前页 ,默认从1开始; size:每页数据条数,默认10
+    upCallback (page, mescroll) {
+      console.log('up')
+      const _this = this
+      // 数据渲染成功后,隐藏下拉刷新的状态
+      setTimeout(function () {
+        _this.getBillDetails(page.num).then(res => {
+          _this.$nextTick(() => {
+            mescroll.endSuccess(res)
+          })
+        })
+      }, 200)
+    },
     pickDate () {
       this.pickerVisible = true
     },
@@ -106,7 +160,8 @@ export default {
     confirm (res) {
       this.result = res[0].value + '-' + res[1].value
       this.dateText = res[0].value + '年' + res[1].value + '月'
-      this.getBillDetails()
+      this.billData = []
+      this.mescroll.resetUpScroll()
     },
     getPickerYearList () {
       const nowYear = new Date().getFullYear()
@@ -122,15 +177,20 @@ export default {
       this.pickData[0] = yearList
     },
     // 获取账单列表
-    getBillDetails () {
-      const params = {
-        createTime: this.result,
-        pageNo: this.pageNo,
-        pageSize: this.pageSize
-      }
-      billDetails(params).then(res => {
-        const resData = res.data.data
-        this.billData = resData.list
+    getBillDetails (pageNo) {
+      return new Promise((resolve, reject) => {
+        const params = {
+          createTime: this.result,
+          pageNo: pageNo,
+          pageSize: this.pageSize
+        }
+        billDetails(params).then(res => {
+          const resData = res.data.data
+          this.billData = resData.list
+          resolve(resData.list.length)
+        }).catch(err => {
+          reject(err)
+        })
       })
     }
   }
